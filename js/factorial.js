bd2a60cd23c75907449ee1653a14dcb09479cb0f
santorsm <matt.santorsola@gmail.com>
2021-09-29 19:30:44 -0700

    added pseudocode and initital solution

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,22 @@
+
 exports.factorial = function(num) {
+  
+  // define output variable = 1
+  let output = 1
+  // if num <= 1, return output variable
+  if (num <= 1){
+    return output
+  }
+  // else
+  // while num > 1
+  while (num > 1){
+    output *= num
+    num--;
+  }
+  // multiply output by num
+  // decrement num by 1
+  // return output variable
+  return output
+
+ 
 };
